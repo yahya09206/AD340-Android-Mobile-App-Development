b48103e8eb94910190547111819f2b598ec3c687
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-06 20:34:18 -0700

    thinking of adding menu

--- app/src/main/java/com/example/yhussein/helloworld/MainActivity.java
+++ app/src/main/java/com/example/yhussein/helloworld/MainActivity.java
@@ -7,11 +7,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
     }
 
 
