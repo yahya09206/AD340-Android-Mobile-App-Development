475455dfd4652a039d6e34eb7db437c72d5200c3
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-06 20:09:26 -0700

    finally getting it

--- app/src/main/java/com/example/yhussein/helloworld/MainActivity.java
+++ app/src/main/java/com/example/yhussein/helloworld/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.yhussein.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = findViewById(R.id.name);
     }
+
+
 }
