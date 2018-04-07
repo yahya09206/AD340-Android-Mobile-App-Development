d69cd26886d9aaca2367d313a066d6fe2375f77b
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-06 20:24:57 -0700

    got text to show up

--- app/src/main/java/com/example/yhussein/helloworld/MainActivity.java
+++ app/src/main/java/com/example/yhussein/helloworld/MainActivity.java
@@ -2,17 +2,16 @@ package com.example.yhussein.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        name = findViewById(R.id.name);
     }
 
 
