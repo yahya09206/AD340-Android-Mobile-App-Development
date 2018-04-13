0ee5733de1f30b0c1cb8bf7e4089075175245bd8
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-12 23:48:02 -0700

    set up menu

--- app/src/main/java/com/example/yhussein/helloworld/MainActivity.java
+++ app/src/main/java/com/example/yhussein/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.yhussein.helloworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 //option + enter for creating test file
@@ -23,4 +24,12 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.infoid){
+            textView.setText(R.string.info);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
