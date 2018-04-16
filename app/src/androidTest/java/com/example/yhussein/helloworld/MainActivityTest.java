a5e54287b7bbfdd8037a190fe848813a892cc843
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-16 00:16:49 -0700

    working on tests

--- app/src/androidTest/java/com/example/yhussein/helloworld/MainActivityTest.java
+++ app/src/androidTest/java/com/example/yhussein/helloworld/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.example.yhussein.helloworld;
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -8,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -20,6 +22,13 @@ public class MainActivityTest {
 
     @Test
     public void fullinfo(){
+        //check to see if text matches from main screen
+        onView(withId(R.id.textView))
+                .check(matches(withText(R.string.hello)));
+
+        //onclick
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+
         //check to see if text string matches the one on the menu
         onView(withId(R.id.textView))
                 .check(matches(withText(R.string.info)));
