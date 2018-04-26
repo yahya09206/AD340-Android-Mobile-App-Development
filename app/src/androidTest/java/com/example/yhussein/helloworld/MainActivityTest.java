641ef4ff9186bdafeb00012ac160af24dd88ce07
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-25 22:10:59 -0700

    checking if email matches

--- app/src/androidTest/java/com/example/yhussein/helloworld/MainActivityTest.java
+++ app/src/androidTest/java/com/example/yhussein/helloworld/MainActivityTest.java
@@ -1,19 +1,27 @@
 package com.example.yhussein.helloworld;
 
-import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static android.support.test.espresso.action.ViewActions.click;
+import static org.hamcrest.Matchers.allOf;
+
 
 
 @RunWith(AndroidJUnit4.class)
@@ -23,8 +31,11 @@ public class MainActivityTest {
     public ActivityTestRule<MainActivity> testRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void fullinfo(){
+    public void fullInfo(){
 
+        ViewInteraction nameET = onView(
+                allOf(withId(R.id.email), withText("Email"), isDisplayed()));
+        nameET.check(matches(isDisplayed()));
     }
 
 }
