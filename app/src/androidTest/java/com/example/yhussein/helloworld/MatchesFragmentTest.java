eb7ebc9fb86ff546583c9cbfa1b8faa371ebb142
Yahya Hussein <yahyahussein91@gmail.com>
2018-06-13 16:04:24 -0700

    cleaned up

--- app/src/androidTest/java/com/example/yhussein/helloworld/MatchesFragmentTest.java
+++ app/src/androidTest/java/com/example/yhussein/helloworld/MatchesFragmentTest.java
@@ -12,7 +12,6 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 public class MatchesFragmentTest {
@@ -24,8 +23,6 @@ public class MatchesFragmentTest {
     public void checkListViewIsVisible() {
         Espresso.onView(ViewMatchers.withId(R.id.matchList)).check(matches(isDisplayed()));
     }
-
-
 
 
 }
